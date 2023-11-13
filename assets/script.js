62d4ff1b662c9353d7c2af77473060598ce57b35
umnovjp <umnovjp@yahoo.com>
2023-11-12 22:54:20 -0600

    productive update tonight getting staff started

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-var scheduleContent = document.getElementById('schedule'); var gameId; var inputVal = '2021';
+// var scheduleContent = document.getElementById('schedule'); var gameId; var startValue = '2021';
+// var formattedStart; var formattedEnd
+var startYear; var endYear
+// lines below will
 
-// lines below will allow user to search by year
-function getInputValue() {
-  var inputVal = document.getElementById('datepicker').value; var date = inputVal.split('/');
-  var formatted = date[2] + '-' + date[0] + '-' + date[1]; console.log(formatted)
+function getStartValue() {  
+var endValue = document.getElementById('datepickerStart').value; // var endDate = endValue.split('/');
+var startValue = document.getElementById('datepickerStart').value; // var startDate =startValue.split('/');
+// var formattedStart = startDate[2] + '-' + startDate[0] + '-' + startDate[1];
+//   var formattedEnd = endDate[2] + '-' + endDate[0] + '-' + endDate[1]; 
+  console.log(startValue, endValue)
   var requestURL = 'https://data.sec.gov/submissions/CIK0001517228.json'// + formatted;
   fetch(requestURL, {
     "method": "GET", "headers": {}
@@ -12,7 +17,11 @@ function getInputValue() {
       return response.json();
     })
     .then(function (data) {
-     console.log(data.filings);
+     console.log(data.filings); var dates = data.filings.recent.filingDate
+     for (i = 0; i < dates.length/100; i++) {startDateArray = startValue.split('-'); endDateArray = endValue.split('-')
+     startYear = startDateArray[0]; endYear = endDateArray[0]; startMonth = startDateArray[1]; endMonth = endDateArray[1]; startDate = startDateArray[2]; endDate = endDateArray[2]
+     // if loop here
+     console.log(startYear)}
     } // end second .then from getinputvalue
     );
-} // end getInput Value function $65k at 4.50% on 3/27
+} // end getStartValue function
