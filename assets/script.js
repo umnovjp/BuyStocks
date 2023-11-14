d94602ac85b7ed8096145962bffa9e2a9c582d2c
umnovjp <umnovjp@yahoo.com>
2023-11-13 23:10:02 -0600

    retrieving first data

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,13 @@ var startYear; var endYear
 // lines below will
 
 function getStartValue() {  
-var endValue = document.getElementById('datepickerStart').value; // var endDate = endValue.split('/');
-var startValue = document.getElementById('datepickerStart').value; // var startDate =startValue.split('/');
-// var formattedStart = startDate[2] + '-' + startDate[0] + '-' + startDate[1];
-//   var formattedEnd = endDate[2] + '-' + endDate[0] + '-' + endDate[1]; 
+var endValue = document.getElementById('datepickerStart').value;
+var startValue = document.getElementById('datepickerStart').value; 
   console.log(startValue, endValue)
+  startValueArray = startValue.split('-');
+  endValueArray = endValue.split('-');
   var requestURL = 'https://data.sec.gov/submissions/CIK0001517228.json'// + formatted;
-  fetch(requestURL, {
-    "method": "GET", "headers": {}
+  fetch(requestURL, {"method": "GET", "headers": {}
   })
     .then(function (response) {
       return response.json();
@@ -20,8 +19,11 @@ var startValue = document.getElementById('datepickerStart').value; // var startD
      console.log(data.filings); var dates = data.filings.recent.filingDate
      for (i = 0; i < dates.length/100; i++) {startDateArray = startValue.split('-'); endDateArray = endValue.split('-')
      startYear = startDateArray[0]; endYear = endDateArray[0]; startMonth = startDateArray[1]; endMonth = endDateArray[1]; startDate = startDateArray[2]; endDate = endDateArray[2]
-     // if loop here
-     console.log(startYear)}
+     // if loop will be here
+     const filings = [];
+     if ((startYear >= starValueArray[0])&&(startMonth >=startvalueArray[1])&&(startDate >=startValueArray[2])&&(endYear <=endValueArray[0])&&(endMonth<=endValueArray[1])&&(endDate<=endValueArray[2]))
+     {filings.push(i)}
+     console.log(filings)}
     } // end second .then from getinputvalue
     );
 } // end getStartValue function
