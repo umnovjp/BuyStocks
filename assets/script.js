13f0de8fb6f360690c40adc7f36a8b6b2ef57057
umnovjp <umnovjp@yahoo.com>
2023-11-14 23:25:58 -0600

    for some reason all events are added to array

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ var startValue = document.getElementById('datepickerStart').value;
   console.log(startValue, endValue)
   startValueArray = startValue.split('-');
   endValueArray = endValue.split('-');
+  console.log(startValueArray, endValueArray)
   var requestURL = 'https://data.sec.gov/submissions/CIK0001517228.json'// + formatted;
   fetch(requestURL, {"method": "GET", "headers": {}
   })
@@ -16,14 +17,14 @@ var startValue = document.getElementById('datepickerStart').value;
       return response.json();
     })
     .then(function (data) {
-     console.log(data.filings); var dates = data.filings.recent.filingDate
-     for (i = 0; i < dates.length/100; i++) {startDateArray = startValue.split('-'); endDateArray = endValue.split('-')
+     console.log(data.filings); var dates = data.filings.recent.filingDate; const filings = [];
+     for (i = 0; i < dates.length; i++) {startDateArray = startValue.split('-'); endDateArray = endValue.split('-')
      startYear = startDateArray[0]; endYear = endDateArray[0]; startMonth = startDateArray[1]; endMonth = endDateArray[1]; startDate = startDateArray[2]; endDate = endDateArray[2]
-     // if loop will be here
-     const filings = [];
-     if ((startYear >= starValueArray[0])&&(startMonth >=startvalueArray[1])&&(startDate >=startValueArray[2])&&(endYear <=endValueArray[0])&&(endMonth<=endValueArray[1])&&(endDate<=endValueArray[2]))
+     // if loop will be here     
+     if ((startYear >= startValueArray[0])&&(startMonth >=startValueArray[1])&&(startDate >=startValueArray[2])&&(endYear <=endValueArray[0])&&(endMonth<=endValueArray[1])&&(endDate<=endValueArray[2]))
      {filings.push(i)}
-     console.log(filings)}
+     }
+     console.log(filings)
     } // end second .then from getinputvalue
     );
 } // end getStartValue function
